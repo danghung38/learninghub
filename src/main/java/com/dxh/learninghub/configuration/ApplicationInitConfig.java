260256bd91e19b8fa83295b08f363d8413ee3dac
hungdx.aivory <hungdx.intern@aivory.app>
2026-08-15 17:03:03 +0700

    refactor: refactor admin account to .env

--- src/main/java/com/dxh/learninghub/configuration/ApplicationInitConfig.java
+++ src/main/java/com/dxh/learninghub/configuration/ApplicationInitConfig.java
@@ -99,7 +99,7 @@ public class ApplicationInitConfig {
                         .build();
 
                 userRepository.save(user);
-                log.warn("admin user has been created with default password: admin, please change it");
+                log.warn("admin user has been created with default password, please change it");
             }
         };
     }
